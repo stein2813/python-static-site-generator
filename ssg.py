049ed681b673acabd0b88335a677656f641d61a1
Mark Steinhelper <steinhm1@nationwide.com>
2022-04-22 13:49:45 -0400

    Site Class step one

---
+++ ssg.py
@@ -0,0 +1,12 @@
+import typer
+from ssg import Site
+
+def main(source="content", dest="dist"):
+    config = {
+        "source": source,
+        "dest": dest
+    }
+    
+    Site(**config).build()
+    
+typer.run()
